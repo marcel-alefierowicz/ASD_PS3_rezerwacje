820b794df0a4004e06341a7941cae235b66c25a2
marcel-alefierowicz <mrclalefierowicz@gmail.com>
2025-11-18 00:46:02 +0100

    vibecoded some bullshit but it works for 60%

--- Program.cs
+++ Program.cs
@@ -1,19 +1,60 @@
-﻿class Program
+﻿using System.Diagnostics;
+
+class Program
 {
-    static int getReservations(string[] data)
+    struct Reservation
     {
-        int n, p, k, z;
-        for (int i = 0; i < data.Length; i++)
+        public int p, k, z;
+        public Reservation(int p, int k, int z)
         {
-            System.Console.WriteLine(data[i]);
+            this.p = p;
+            this.k = k;
+            this.z = z;
         }
-        int.TryParse(data[0], out n);
-        return 0;
     }
 
-    static int Main(string[] args)
+    static void Main()
     {
-        getReservations(File.ReadAllLines("./in/in1.txt"));
-        return 0;
+        string[] data = File.ReadAllLines("./in/in1.txt");
+        Stopwatch s = new();
+        s.Start();
+        int n = int.Parse(data[0]);
+        List<Reservation> reservations = new List<Reservation>();
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            string[] parts = data[i].Split(' ');
+
+            int p = int.Parse(parts[0]);
+            int k = int.Parse(parts[1]);
+            int z = int.Parse(parts[2]);
+
+            reservations.Add(new Reservation(p, k, z));
+        }
+
+        int T = reservations.Max(r => r.k);
+
+        List<Reservation>[] ending = new List<Reservation>[T + 1];
+        for (int t = 0; t <= T; t++)
+            ending[t] = new List<Reservation>();
+
+        foreach (var r in reservations)
+            ending[r.k].Add(r);
+
+        int[] Z = new int[T + 1];
+        Z[0] = 0;
+
+        for (int t = 1; t <= T; t++)
+        {
+            Z[t] = Z[t - 1];
+
+            foreach (var r in ending[t])
+            {
+                Z[t] = Math.Max(Z[t], Z[r.p] + r.z);
+            }
+        }
+        s.Stop();
+        System.Console.WriteLine($"elapsed: {s.ElapsedMilliseconds} ms");
+        Console.WriteLine(Z[T]);
     }
 }
