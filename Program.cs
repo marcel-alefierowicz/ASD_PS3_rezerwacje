344a4acfa5ed5fc67d69ed647349b9822f60700b
marcel-alefierowicz <mrclalefierowicz@gmail.com>
2025-11-23 19:50:17 +0100

    WIP: improved algo to work for potentially scalable number of rooms

--- Program.cs
+++ Program.cs
@@ -4,36 +4,18 @@ class Program
 {
     struct Reservation
     {
-        public int p, k, z;
-        public Reservation(int p, int k, int z)
+        public int p, k, z, id;
+        public Reservation(int p, int k, int z, int id)
         {
             this.p = p;
             this.k = k;
             this.z = z;
+            this.id = id;
         }
     }
-
-    static void Main()
+    static int profit(List<Reservation> reservations, ref List<int> excluded)
     {
-        string[] data = File.ReadAllLines("./in/in1.txt");
-        Stopwatch s = new();
-        s.Start();
-        int n = int.Parse(data[0]);
-        List<Reservation> reservations = new List<Reservation>();
-
-        for (int i = 1; i < n + 1; i++)
-        {
-            string[] parts = data[i].Split(' ');
-
-            int p = int.Parse(parts[0]);
-            int k = int.Parse(parts[1]);
-            int z = int.Parse(parts[2]);
-
-            reservations.Add(new Reservation(p, k, z));
-        }
-
         int T = reservations.Max(r => r.k); // najpozniej konczaca sie rezerwacja
-
         List<Reservation>[] ending = new List<Reservation>[T + 1]; // lista rezerwacji grupowana po ich godzinie konca
         for (int t = 0; t <= T; t++)
             ending[t] = new List<Reservation>();
@@ -50,11 +32,47 @@ class Program
 
             foreach (var r in ending[t])
             {
-                Z[t] = Math.Max(Z[t], Z[r.p] + r.z);
+                if (!excluded.Contains(r.id))
+                {
+                    if (Z[r.p] + r.z > Z[t])
+                    {
+                        Z[t] = Z[r.p] + r.z;
+                        // THIS IS PREMATURE, TODO FIX !!!!!!!!!!!!!!!!!!!!!!!!!!
+                        excluded.Add(r.id);
+                    }
+                }
             }
         }
+        return Z[T];
+    }
+
+    static void Main()
+    {
+        string[] data = File.ReadAllLines("./in/in1_full.txt");
+
+        Stopwatch s = new();
+        s.Start();
+
+        int n = int.Parse(data[0]);
+        List<Reservation> reservations = new();
+        List<int> excluded = new();
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            string[] parts = data[i].Split(' ');
+
+            int p = int.Parse(parts[0]);
+            int k = int.Parse(parts[1]);
+            int z = int.Parse(parts[2]);
+
+            reservations.Add(new Reservation(p, k, z, i));
+        }
+
+        int pokoj1 = profit(reservations, ref excluded);
+        int pokoj2 = profit(reservations, ref excluded);
+        int output = pokoj1 + pokoj2;
         s.Stop();
+        System.Console.WriteLine($"Zysk:{output}");
         Console.WriteLine($"elapsed: {s.ElapsedMilliseconds} ms");
-        Console.WriteLine(Z[T]);
     }
 }
