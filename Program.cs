149addb8069887c9adfcfc4ccecb1bcdd1f72afc
marcel-alefierowicz <mrclalefierowicz@gmail.com>
2025-11-25 00:07:34 +0100

    bugfix - re-added parent table for backtracking fixing infinite loop

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 class Program
 {
@@ -13,64 +17,16 @@ class Program
             this.id = id;
         }
     }
-    static int profit(List<Reservation> reservations, ref List<int> excluded)
-    {
-        int T = reservations.Max(r => r.k);
-
-        List<Reservation>[] ending = new List<Reservation>[T + 1];
-        for (int t = 0; t <= T; t++) ending[t] = new();
-        foreach (var r in reservations) ending[r.k].Add(r);
-
-        int[] Z = new int[T + 1];
-        int[] picked = new int[T + 1];
-
-        Z[0] = 0;
-        picked[0] = -1;
-
-        for (int t = 1; t <= T; t++)
-        {
-            Z[t] = Z[t - 1];
-            picked[t] = -1;
-
-            foreach (var r in ending[t])
-            {
-                if (Z[r.p] + r.z > Z[t])
-                {
-                    Z[t] = Z[r.p] + r.z;
-                    picked[t] = r.id;
-                }
-            }
-        }
-
-        int cur = T;
-        while (cur > 0)
-        {
-            if (picked[cur] != -1)
-            {
-                var reservation = reservations[picked[cur]];
-                cur = reservation.p;
-            }
-            else --cur;
-        }
-
-        return Z[T];
-    }
 
     static void Main()
     {
         string[] data = File.ReadAllLines("./in/in1_full.txt");
-
-        Stopwatch s = new();
-        s.Start();
-
         int n = int.Parse(data[0]);
-        List<Reservation> reservations = new();
-        List<int> excluded = new();
+        List<Reservation> reservations = new List<Reservation>();
 
-        for (int i = 1; i < n + 1; i++)
+        for (int i = 1; i <= n; i++)
         {
-            string[] parts = data[i].Split(' ');
-
+            var parts = data[i].Split(' ');
             int p = int.Parse(parts[0]);
             int k = int.Parse(parts[1]);
             int z = int.Parse(parts[2]);
@@ -78,11 +34,67 @@ class Program
             reservations.Add(new Reservation(p, k, z, i));
         }
 
-        var room1 = profit(reservations, ref excluded);
-        var room2 = profit(reservations, ref excluded);
+        Stopwatch s = new Stopwatch();
+        s.Start();
+
+        (int profit_r1, List<int> r1_ids) = profit(reservations);
+
+        List<Reservation> remaining = reservations.Where(r => !r1_ids.Contains(r.id)).ToList();
+
+        (int profit_r2, List<int> r2_ids) = profit(remaining);
 
         s.Stop();
-        Console.WriteLine($"Zysk:{room1 + room2}");
+
+        Console.WriteLine($"out: {profit_r1 + profit_r2}");
         Console.WriteLine($"elapsed: {s.ElapsedMilliseconds} ms");
+    }
+
+    static (int, List<int>) profit(List<Reservation> reservations)
+    {
+        if (reservations == null || reservations.Count == 0)
+            return (0, []);
+
+        int T = reservations.Max(r => r.k);
+
+        List<Reservation>[] ending = new List<Reservation>[T + 1];
+        for (int t = 0; t <= T; t++) ending[t] = [];
+        foreach (var r in reservations) ending[r.k].Add(r);
+
+        int[] Z = new int[T + 1];
+        int[] parent = new int[T + 1]; // spamiętujemy z.p z porownania nizej (Z[z.p] + z.r) 
+        int[] picked = new int[T + 1];
+
+        Z[0] = 0;
+        parent[0] = -1;
+        picked[0] = -1;
+
+        for (int t = 1; t <= T; t++)
+        {
+            Z[t] = Z[t - 1];
+            parent[t] = t - 1;
+            picked[t] = -1;
+
+            foreach (var r in ending[t])
+            {
+                int val = Z[r.p] + r.z;
+                if (val > Z[t])
+                {
+                    Z[t] = val;
+                    parent[t] = r.p;
+                    picked[t] = r.id;
+                }
+            }
+        }
+
+        List<int> chosen = new();
+        int cur = T;
+        while (cur > 0)
+        {
+            if (picked[cur] != -1)
+                chosen.Add(picked[cur]);
+            cur = parent[cur]; // cofamy sie do z.p bo nie ma zadnych innych w czasie trwania rezerwacji wiec jest nieco szybciej
+        }
+
+        return (Z[T], chosen);
     }
 }
